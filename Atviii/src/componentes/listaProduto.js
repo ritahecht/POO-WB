c5225f5439194b45f2dd626f5a7f199427f40579
Rita Hecht <89950512+ritahecht@users.noreply.github.com>
2023-06-15 10:20:53 -0300

    Add files via upload

---
+++ Atviii/src/componentes/listaProduto.js
@@ -0,0 +1,17 @@
+
+
+export default function ListaProduto (props){
+    const estilo = `collection-item active ${props.tema}`
+
+    return (
+        <div className="collection">
+            <a className="collection-item">Cliente 1</a>
+            <a className={estilo}>Cliente 2</a>
+            <a className="collection-item">Cliente 3</a>
+            <a className="collection-item">Cliente 4</a>
+        </div>
+    )
+}
+
+
+  
